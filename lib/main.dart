6622f69d473626e8d56ab5a172502b1ae51ae2df
Syak <kpuccmajl@gmail.com>
2023-11-16 22:32:38 +0400

    lecture no3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -33,16 +31,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  Color _color = Colors.orangeAccent;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-      _color =
-          Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
-    });
-  }
+  final Color _color = Colors.orangeAccent;
 
   @override
   Widget build(BuildContext context) {
@@ -51,30 +40,140 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: _color,
         title: Text(widget.title),
       ),
-      body: Center(
+      body: const MyWidget(),
+    );
+  }
+}
+
+class _CardData {
+  final String text;
+  final String descriptionText;
+  final IconData icon;
+  final String? imageUrl;
+
+  _CardData(
+    this.text, {
+    required this.descriptionText,
+    this.icon = Icons.ac_unit_outlined,
+    this.imageUrl,
+  });
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key}); // КЛЮЧИ
+
+  @override
+  Widget build(BuildContext context) {
+    final data = [
+      _CardData(
+        'Freeze',
+        descriptionText: 'hahaha',
+        imageUrl:
+            'https://www.skedaddlewildlife.com/wp-content/uploads/2018/09/depositphotos_22425309-stock-photo-a-lonely-raccoon-in-winter.jpg',
+      ),
+      _CardData(
+        'Hi',
+        descriptionText: 'ahahahah',
+        icon: Icons.hail,
+        imageUrl:
+            'https://www.thesprucepets.com/thmb/nKNaS4I586B_H7sEUw9QAXvWM_0=/2121x0/filters:no_upscale():strip_icc()/GettyImages-135630198-5ba7d225c9e77c0050cff91b.jpg',
+      ),
+      _CardData(
+        'Oh',
+        descriptionText: 'lol',
+        icon: Icons.warning_amber,
+        imageUrl:
+            'https://furmanagers.com/wp-content/uploads/2019/11/dreamstime_l_22075357.jpg',
+      ),
+    ];
+
+    return Center(
+      child: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            if (_counter > 10)
-              Text(
-                'Why are you clicking?',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-          ],
+          children: data.map((e) => _Card.fromData(e)).toList(),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        backgroundColor: _color,
-        tooltip: 'Increment',
-        child: const Icon(Icons.local_fire_department_sharp),
+    );
+  }
+}
+
+class _Card extends StatelessWidget {
+  final String text;
+  final String descriptionText;
+  final IconData icon;
+  final String? imageUrl;
+
+  const _Card(
+    this.text, {
+    this.icon = Icons.ac_unit_outlined,
+    required this.descriptionText,
+    this.imageUrl,
+  });
+
+  factory _Card.fromData(_CardData data) => _Card(
+        data.text,
+        descriptionText: data.descriptionText,
+        icon: data.icon,
+        imageUrl: data.imageUrl,
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white70,
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: Colors.grey),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(.5),
+            spreadRadius: 4,
+            offset: const Offset(0, 5),
+            blurRadius: 8,
+          ),
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: SizedBox(
+              height: 140,
+              width: 100,
+              child: Image.network(
+                imageUrl ?? '',
+                fit: BoxFit.cover,
+                errorBuilder: (_, __, ___) => const Placeholder(),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    text,
+                    style: Theme.of(context).textTheme.headlineLarge,
+                  ),
+                  Text(
+                    descriptionText,
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Icon(icon),
+          ),
+        ],
       ),
     );
   }
